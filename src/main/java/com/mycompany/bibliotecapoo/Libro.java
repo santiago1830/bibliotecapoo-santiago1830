63aaf3fa2a634cbc8357ff04bb7834dd5e83a633
santiago1830 <146890780+santiago1830@users.noreply.github.com>
2024-06-04 10:25:52 -0500

    Update Libro.java

--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,5 +1,33 @@
 package com.mycompany.bibliotecapoo;
 
 public class Libro {
-    // TODO: Aquí va tu código
+    private int anoPublicacion;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private boolean leido;
+    
+    public Libro(int annioPublicacion, String titulo,String autor, String genero, boolean leido){
+        this.anoPublicacion = annioPublicacion;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.leido = leido;
+    }
+    
+    public String mostrarInformación(){
+        return ("Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", Género: " + genero);
+    }
+    
+    public void marcarLeido(){
+        leido = true;
+    }
+    
+    public boolean esAntiguo(){
+        return (2024 - anoPublicacion > 50);
+    }
+    
+    public boolean getLeido(){
+        return this.leido;
+    }
 }
