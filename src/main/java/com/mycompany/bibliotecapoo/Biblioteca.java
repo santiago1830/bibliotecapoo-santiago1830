b9fff89bba00139f5810649a77d9d931ff451563
santiago1830 <146890780+santiago1830@users.noreply.github.com>
2024-06-04 10:18:54 -0500

    Update Biblioteca.java

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,38 @@
 package com.mycompany.bibliotecapoo;
 
+import java.util.LinkedList;
+
 public class Biblioteca {
-    // TODO: Aquí va tu código
+    private LinkedList<Libro> libros;
+
+    public Biblioteca() {
+        this.libros = new LinkedList<>();
+    }
+
+    public void registrarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    public void mostrarLibros() {
+        for (Libro libro : libros) {
+            System.out.println(libro.mostrarInformación());
+        }
+    }
+
+    public void mostrarLibrosNoLeidos() {
+        for (Libro libro : libros) {
+            if (!libro.getLeido()) {
+                System.out.println(libro.mostrarInformación());
+            }
+        }
+    }
+
+    public Libro buscarLibro(String palabraBusqueda) {
+        for (Libro libro : libros) {
+            if (libro.mostrarInformación().toLowerCase().contains(palabraBusqueda.toLowerCase())) {
+                return libro;
+            }
+        }
+        return null;
+    }
 }
